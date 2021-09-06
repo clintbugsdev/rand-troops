12834426d2d245db9d9f719c18a055eb3133997a
Clinton Bugtong <clintbugs.dev@gmail.com>
2021-09-07 02:22:21 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const crypto = require("crypto");
 const ffi = require("ffi-napi");
 const ref = require("ref-napi");
+
 const arrayType = require("ref-array-di")(ref);
 
 // define the "int[]" type
@@ -9,25 +11,36 @@ const randTroopsArrayPtr = ref.refType(randTroopsArray);
 // Rust library path
 const randGenlibPath = "target/release/librand_gen.so";
 const lib = ffi.Library(randGenlibPath, {
-  rand_generate: ["size_t", ["uint32", "uint32", randTroopsArrayPtr]],
+  rand_generate: ["size_t", ["string", "uint32", "uint32", randTroopsArrayPtr]],
   rand_free: ["void", [randTroopsArray, "size_t"]],
 });
 
-function randGenerate(armyTypes, armySize) {
+function randGenerate(player, armyTypes, armySize) {
+  const reqStr = `${player}${armyTypes.join("")}${armySize}`;
+  const sha256Hasher = crypto.createHmac("sha256", reqStr);
+  const hashedReq = sha256Hasher.update(reqStr).digest("base64");
   const bufPtr = ref.alloc(randTroopsArrayPtr);
-  const arraySize = lib.rand_generate(armyTypes.length, armySize, bufPtr);
+  const arraySize = lib.rand_generate(
+    hashedReq,
+    armyTypes.length,
+    armySize,
+    bufPtr
+  );
   const armies = randTroopsArray(bufPtr.deref());
   armies.length = arraySize;
 
   try {
-    let str = "";
+    let result = [];
     for (let i = 0; i < armies.length; i++) {
-      str += armies[i] + ", ";
+      result.push({ name: armyTypes[i], troops: armies[i] });
     }
-    return str;
+    return result;
   } finally {
     lib.rand_free(armies, arraySize);
   }
 }
 
-console.log(randGenerate(["Spearmen", "Swordsmen", "Archer"], 100));
+let player = "Clinton";
+let armyTypes = ["Spearmen", "Swordsmen", "Archer"];
+let armySize = 4;
+console.log(randGenerate(player, armyTypes, armySize));
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,90 @@
-extern crate libc;
+#![allow(unused)] // silence unused warnings while exploring (to comment out)
 
-use libc::size_t;
+extern crate itertools;
+extern crate libc;
+use libc::{c_char, size_t};
+use rand::seq::SliceRandom;
 use rand::Rng;
+use redis::Commands;
+use std::error::Error;
+use std::ffi::CStr;
 use std::mem;
 
-#[no_mangle]
-pub extern "C" fn rand_generate(mut types: u32, mut size: u32, vec: *mut *mut u32) -> size_t {
-    let mut generated: Vec<u32> = Vec::new();
+use itertools::Itertools;
 
+fn connect() -> redis::Connection {
+    redis::Client::open("redis://127.0.0.1/")
+        .expect("Invalid connection URL")
+        .get_connection()
+        .expect("failed to connect to Redis")
+}
+
+fn rand_get_list(hashed_req_str: &str) -> Vec<String> {
+    let mut conn = connect();
+
+    let list: Vec<String> = conn
+        .smembers(hashed_req_str)
+        .expect("failed to execute SMEMBERS");
+    println!("{:?}", list);
+    list
+}
+
+fn rand_is_member(hashed_req_str: &str, item: &String) -> bool {
+    let mut conn = connect();
+
+    let is_member: bool = redis::cmd("SISMEMBER")
+        .arg(hashed_req_str)
+        .arg(item)
+        .query(&mut conn)
+        .expect("failed to execute SISMEMBER");
+
+    is_member
+}
+
+fn rand_add(hashed_req_str: &str, item: &String) -> Result<(), Box<dyn Error>> {
+    let mut conn = connect();
+
+    let _: () = conn
+        .sadd(hashed_req_str, item)
+        .expect("failed to execute SADD");
+
+    Ok(())
+}
+
+fn rand_del(hashed_req_str: &str) -> Result<(), Box<dyn Error>> {
+    let mut conn = connect();
+
+    let _: () = conn.del(hashed_req_str).expect("failed to execute DEL");
+
+    Ok(())
+}
+
+#[no_mangle]
+pub extern "C" fn rand_generate(
+    hashed_req: *const c_char,
+    mut types: u32,
+    mut size: u32,
+    vec: *mut *mut u32,
+) -> size_t {
+    // get hashed request from C string pointer
+    let c_str = unsafe {
+        assert!(!hashed_req.is_null());
+        CStr::from_ptr(hashed_req)
+    };
+    // set to string
+    let hashed_req_str = c_str.to_str().unwrap();
+    let rand_list = rand_get_list(hashed_req_str);
+
+    // initiate variable for random numbers
+    let orig_types = types.clone();
+    let orig_size = size.clone();
+    let mut generated: Vec<u32> = Vec::new();
     if types < size {
         let mut rng = rand::thread_rng();
+        // loop to get random numbers
         loop {
             if types > 1 {
-                let rand_num = rng.gen_range(1..size - (types - 1));
+                let rand_num = rng.gen_range(1..=size - (types - 1));
                 size -= rand_num;
                 generated.push(rand_num);
             } else {
@@ -21,13 +93,28 @@ pub extern "C" fn rand_generate(mut types: u32, mut size: u32, vec: *mut *mut u3
             types -= 1;
             // end of loop
             if types == 0 {
-                break;
+                // convert numbers to strings
+                let mut merged_generated = generated.clone().iter().join("-");
+                // is member then reset variables to go back to loop
+                if rand_is_member(hashed_req_str, &merged_generated) {
+                    // delete random list if length was equal to the size
+                    if rand_list.len() as u32 >= orig_types {
+                        rand_del(hashed_req_str);
+                    }
+                    types = orig_types;
+                    size = orig_size;
+                    println!("{:?}", generated);
+                    generated.clear();
+                } else {
+                    // add generated random numbers to Redis
+                    rand_add(hashed_req_str, &merged_generated);
+                    break;
+                }
             }
         }
     }
-
-    generated.shrink_to_fit();
     let ret = generated.len();
+    // set generated random numbers as pointer
     unsafe { *vec = generated.as_mut_ptr() };
     mem::forget(generated);
 
@@ -40,7 +127,7 @@ pub extern "C" fn rand_free(arr: *mut u32, arr_size: size_t) {
         if arr.is_null() {
             return;
         }
-
+        // free the generated random numbers array pointer
         Vec::from_raw_parts(arr, arr_size, arr_size)
     };
 }
